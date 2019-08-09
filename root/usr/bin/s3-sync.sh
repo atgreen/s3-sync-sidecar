1a3800ea3c242de5856ee8356c02acba47e14efb
Anthony Green <green@moxielogic.com>
2019-08-09 14:33:50 -0400

    Fix

--- root/usr/bin/s3-sync.sh
+++ root/usr/bin/s3-sync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while true; do
-    [ -v "${S3REMOTEOUTBOX}" ] && [ -v "${LOCALINBOX}" ] && aws sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
-    [ -v "${S3REMOTEINBOX}" ] && [ -v "${LOCALOUTBOX}" ] && aws sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
+    [ ! -v "${S3REMOTEOUTBOX}" ] && [ ! -v "${LOCALINBOX}" ] && aws sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
+    [ ! -v "${S3REMOTEINBOX}" ] && [ ! -v "${LOCALOUTBOX}" ] && aws sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
     sleep 5 ;
 done
 
