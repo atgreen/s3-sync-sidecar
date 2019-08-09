6423ad1a2c67a40efec79befa2edec011bfbc05a
Anthony Green <green@moxielogic.com>
2019-08-09 15:26:15 -0400

    Hacks

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Anthony Green "green@redhat.com"
 RUN yum install -y python3-pip && pip3 install awscli && yum clean all
 COPY ./root /
 
+# Short term hack
+USER 0
+
 CMD ["/usr/bin/s3-sync.sh"]
-
-
-
--- root/usr/bin/s3-sync.sh
+++ root/usr/bin/s3-sync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while true; do
-    [ ! -v "${S3REMOTEOUTBOX}" ] && [ ! -v "${LOCALINBOX}" ] && aws s3 sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
-    [ ! -v "${S3REMOTEINBOX}" ] && [ ! -v "${LOCALOUTBOX}" ] && aws s3 sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
+    [ ! -v "${S3REMOTEOUTBOX}" ] && [ ! -v "${LOCALINBOX}" ] || aws s3 sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
+    [ ! -v "${S3REMOTEINBOX}" ] && [ ! -v "${LOCALOUTBOX}" ] || aws s3 sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
     sleep 5 ;
 done
 
