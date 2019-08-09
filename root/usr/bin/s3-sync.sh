f7eb0380eb942e78316f049835355191f39cceb9
Anthony Green <green@moxielogic.com>
2019-08-09 12:41:31 -0400

    Fix arg order

--- root/usr/bin/s3-sync.sh
+++ root/usr/bin/s3-sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-    [ -v "${S3REMOTEOUTBOX}" ] && [ -v "${LOCALINBOX}" ]  && aws sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
-    [ -v "${S3REMOTEINBOX}" ] && [ -v "${LOCALOUTBOX}" ]  && aws sync --delete ${S3REMOTEINBOX} ${LOCALOUTBOX} ;
+    [ -v "${S3REMOTEOUTBOX}" ] && [ -v "${LOCALINBOX}" ] && aws sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
+    [ -v "${S3REMOTEINBOX}" ] && [ -v "${LOCALOUTBOX}" ] && aws sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
     sleep 5 ;
 fi
