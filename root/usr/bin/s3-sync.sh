3ce9ab1b0079ccf3effe1d204bd2d12cb6591f69
Anthony Green <green@moxielogic.com>
2019-08-09 14:04:09 -0400

    Fyx tipo

--- root/usr/bin/s3-sync.sh
+++ root/usr/bin/s3-sync.sh
@@ -4,4 +4,5 @@ while true; do
     [ -v "${S3REMOTEOUTBOX}" ] && [ -v "${LOCALINBOX}" ] && aws sync --delete ${S3REMOTEOUTBOX} ${LOCALINBOX} ;
     [ -v "${S3REMOTEINBOX}" ] && [ -v "${LOCALOUTBOX}" ] && aws sync --delete ${LOCALOUTBOX} ${S3REMOTEINBOX} ;
     sleep 5 ;
-fi
+done
+
